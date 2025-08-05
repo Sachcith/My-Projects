3b52d9742b836db0f0bd22701236db5bee673537
Sachcith <sachcith@gmail.com>
2025-08-05 21:51:31 +0530

    Connect 4 Class Added

--- Python Connect 4/Backtracking.py
+++ Python Connect 4/Backtracking.py
@@ -11,7 +11,10 @@ class Board:
     def disp(self):
         for i in self.board:
             for j in i:
-                print(j,end=" ")
+                if j!=0:
+                    print(j,end=" ")
+                else:
+                    print("-",end=" ")
             print()
         print()
 
@@ -29,11 +32,11 @@ class Board:
         self.board[self.height-self.valid[column]-1][column]=0
         return True
     
-    def heurSupport(self,column):
+    def heurSupport(self,column,p=False):
         num = {"X":0,"O":0,0:0}
         pos = {0:0,1:0,2:0,3:0,4:0}
-        x = [[-1,-2,-3],[1,2,3],[-1,-2,-3],[0,0,0],[1,2,3],[0,0,0],[1,2,3]]
-        y = [[-1,-2,-3],[0,0,0],[1,2,3],[1,2,3],[1,2,3],[-1,-2,-3],[-1,-2,-3]]
+        x = [[-1,-2,-3],[0,0,0],[1,2,3],[1,2,3],[1,2,3],[0,0,0],[-1,-2,-3]]
+        y = [[-1,-2,-3],[-1,-2,-3],[-1,-2,-3],[0,0,0],[1,2,3],[1,2,3],[1,2,3]]
         index = self.height - self.valid[column]
         temp = self.board[index][column]
         for i in range(7):
@@ -51,6 +54,30 @@ class Board:
                         break
             if flag:
                 pos[num[temp]]+=1
+        
+        seven = [0 for i in range(7)]
+        for i in range(7):
+            num = {"X":0,"O":0,0:0}
+            num[temp]=1
+            flag = True
+            for j in range(3):
+                y1 = column + y[i][j]
+                x1 = index + x[i][j]
+                if x1>=0 and y1>=0 and x1<self.height and y1<self.width:
+                    if self.board[x1][y1]==temp:
+                        num[self.board[x1][y1]]+=1
+                    else:
+                        break
+            seven[i]=num[temp]
+        xtemp = [0,1,2]
+        for i in range(3):
+            if seven[xtemp[i]]+seven[xtemp[i]+4]-1>=3:
+                if seven[xtemp[i]]+seven[xtemp[i]+4]-1>3:
+                    pos[4]+=1
+                else:
+                    pos[3]+=1
+        if p:
+            print(seven)
         return pos
     
     def score(self,pos):
@@ -70,14 +97,16 @@ class Board:
             for i in range(4):
                 heur[i] = heur[i+1]
             heur[4]=0
-            ans = ans - self.score(heur)
+            #ans = ans - self.score(heur)
+            ans = ans - self.score(self.heurSupport(column))
         else:
             self.insert(column,"X")
             heur = self.heurSupport(column)
             for i in range(4):
                 heur[i] = heur[i+1]
             heur[4]=0
-            ans = self.score(heur) - ans
+            #ans = self.score(heur) - ans
+            ans = ans - self.score(self.heurSupport(column))
         self.delete(column)
         self.insert(column,temp)
         return ans
@@ -89,16 +118,16 @@ class Connect4:
         self.__board = Board()
         self.__player = True
 
-    def next_move(self,player,cur_depth,max_depth,column):
+    def next_move(self,player,cur_depth,max_depth,column=0):
         if cur_depth == max_depth:
             return self.__board.heuristic(column),column
         if player:
             ma = float('-inf')
             index = -1
-            for i in range(1,7+1):
-                if self.__board.insert("X",i):
+            for i in range(7):
+                if self.__board.insert(i,"X"):
                     temp,j = self.next_move(False,cur_depth+1,max_depth,i)
-                    if temp > ma:
+                    if temp >= ma:
                         ma = temp
                         index = i
                     self.__board.delete(i)
@@ -106,10 +135,10 @@ class Connect4:
         else:
             mi = float('inf')
             index = -1
-            for i in range(1,7+1):
-                if self.__board.insert("X",i):
-                    temp,j = self.next_move(False,cur_depth+1,max_depth,i)
-                    if temp < mi:
+            for i in range(7):
+                if self.__board.insert(i,"O"):
+                    temp,j = self.next_move(True,cur_depth+1,max_depth,i)
+                    if temp <= mi:
                         mi = temp
                         index = i
                     self.__board.delete(i)
@@ -118,11 +147,48 @@ class Connect4:
 
     def start_game(self):
         print("Game Started")
+        count = 42
+        while count!=0:
+            count-=1
+            move = -1
+            if self.__player:
+                self.__player = False
+                temp = int(input("Enter a value from 1-7: "))
+                move = temp
+                while temp>6 or temp<0:
+                    print(temp,"is not in range.")
+                    temp = int(input("Enter a value from 1-7: "))
+                    move = temp
+                temp = self.__board.insert(temp,"X")
+                while temp!=True:
+                    print("Column alread full")
+                    temp = int(input("Enter a value from 1-7: "))
+                    move = temp
+                    while temp>6 or temp<0:
+                        print(temp,"is not in range.")
+                        temp = int(input("Enter a value from 1-7: "))
+                        move = temp
+                    temp = self.__board.insert(temp,"X")
+            else:
+                self.__player = True
+                val,index = self.next_move(False,0,5)
+                self.__board.insert(index,"O")
+                move = index
+            self.__board.heurSupport(move,True)
+            self.__board.disp()
+            final = self.__board.heuristic(move)
+            if final>=10000:
+                print("X won")
+                break
+            elif final<=-10000:
+                print("O won")
+                break
+        print("Game Ended!!!")
+            
 
 
 
-
-board = Board()
+'''board = Board()
 board.disp()
 board.insert(1,"O")
 board.insert(2,"O")
@@ -131,10 +197,31 @@ board.insert(4,"X")
 board.insert(4,"X")
 board.insert(4,"X")
 board.insert(4,"O")
-'''for i in range(2):
-    print(board.delete(1))'''
+"""for i in range(2):
+    print(board.delete(1))"""
 board.disp()
 print(board.heurSupport(4))
 print(board.heuristic(4))
+'''
+
+x = [[-1,-2,-3],[0,0,0],[1,2,3],[1,2,3],[1,2,3],[0,0,0],[-1,-2,-3]]
+y = [[-1,-2,-3],[-1,-2,-3],[-1,-2,-3],[0,0,0],[1,2,3],[1,2,3],[1,2,3]]
+a = [["-" for i in range(7)] for j in range(7)]
+
+for i in range(7):
+    for j in range(3):
+        y1 = 3 + y[i][j]
+        x1 = 3 + x[i][j]
+        print(x1,y1,i)
+        if x1>=0 and y1>=0 and x1<7 and y1<7:
+            a[x1][y1]=i
+
+for i in a:
+    for j in i:
+        print(j,end=" ")
+    print()
+print()
 
 temp = Connect4()
+temp.start_game()
+
